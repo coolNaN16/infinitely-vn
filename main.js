e3a5c42473966507de00ca207f9e4ca2ba4a2048
coolNaN16 <kenzie.cool16.3@gmail.com>
2026-06-15 20:28:23 +0700

    demo

--- main.js
+++ main.js
@@ -227,6 +227,7 @@ class Scene {
                     currentBgMusic = this.music
 
                     bgMusic.currentTime = 0
+                    console.log("MUSIC STARTS: ", currentBgMusic)
                     bgMusic.play()
 
                     fadeToVolume()
@@ -279,33 +280,64 @@ const haibara2 = new Character("Haibarae", {
 
 
 const scenes = {
-    "start" : new Scene("assets/bg/conan.jpg", "assets/audio/chill.mp3", [
+    "start" : new Scene("assets/bg/beika.jpg", "assets/audio/chill.mp3", [
+        null,
+        null,
+        null,
+        null,
         haibara
     ], [
-        new Dialogue(haibara, "Hello there!", null, "neutral"),
+        new Dialogue(haibara, "Hello there, welcome to <b>Beika!</b>", null, "neutral"),
         new Dialogue(haibara, "My name is <b>Haibara Ai</b>!", null, "neutral"),
-        new Dialogue(haibara, "Nice to meet you!", null, "neutral"),
-        new Dialogue(haibara, "You must be new here, ", null, "neutral"),
-        new Dialogue(haibara, "Let me take you to a tour, ", null, "neutral"),
+        new Dialogue(haibara, "And im gonna be your tour guide for the day!", null, "neutral"),
+        new Dialogue(haibara, "Let's get started!, ", null, "neutral"),
         new Dialogue(haibara, "Follow me!", null, "neutral"),
         new Dialogue(haibara, " ", null, "neutral", "MouriAgency"),
     ]),
-    "MouriAgency" : new Scene("assets/bg/mouri.jpg", "assets/audio/tense.mp3", [
+    "MouriAgency" : new Scene("assets/bg/mouri2.png", "assets/audio/chill.mp3", [
         haibara,
         null,
         null,
         null
     ],[
-        new Dialogue(haibara, "This is the Mouri Detective Agency!", null, "neutral"),
-        new Dialogue(haibara, "Mr. Kogoro lives here along with his daughter Ran, ", null, "neutral"),
-        new Dialogue(haibara, "Conan also lives here!", null, "neutral"),
-        new Dialogue(haibara, "Where do you wanna go now?, ", 
+        new Dialogue(haibara, "This is <b>Detective Mouri's Detective Agency</b>!", null, "neutral"),
+        new Dialogue(haibara, "<b>Detective Mouri</b> works and lives here along with his daughter <b>Ran</b>, ", null, "neutral"),
+        new Dialogue(haibara, "Oh yeah, <b>Conan</b> also lives here!", null, "neutral"),
+        new Dialogue(haibara, "People usually go here to resolve their <b>problems</b>, ", null, "neutral"),
+        new Dialogue(haibara, "Like seriously, sometimes it's not even a case!, ", null, "neutral"),
+        new Dialogue(haibara, "Anyways... lets continue! ", null, "neutral"),
+        new Dialogue(haibara, "Soo, where do you wanna go now?, ", 
             new Choice([{
-                "text" : "Go back",
-                "outcome" : () => {changeNextScene("start")}
+                "text" : "Agasa's House",
+                "outcome" : () => {changeNextScene("AgasaHouse")}
             }, {
-                "text" : "Stay here",
+                "text" : "Say that again?",
                 "outcome" : () => {changeNextScene("MouriAgency")}
+            }]
+        ), "neutral"),
+        new Dialogue(haibara, "Alright!", null, "neutral"),
+        new Dialogue(haibara, " ", null, "neutral", "start"),
+    ]),
+    "AgasaHouse" : new Scene("assets/bg/agasa.jpg", "assets/audio/chill.mp3", [
+        haibara,
+        null,
+        null,
+        null
+    ],[
+        new Dialogue(haibara, "Here it is!", null, "neutral"),
+        new Dialogue(haibara, "The <b>myth</b>, ", null, "neutral"),
+        new Dialogue(haibara, "The <b>legend</b>, ", null, "neutral"),
+        new Dialogue(haibara, "<b>My House!!!!!</b>, ", null, "neutral"),
+        new Dialogue(haibara, "Well, not really my house i guess, it's <b>Professor Agasa's House!!!</b>", null, "neutral"),
+        new Dialogue(haibara, "Located at <b>2-22, Beika Town, Beika City,</b>", null, "neutral"),
+        new Dialogue(haibara, "This is where I live after i shrunk into <b>Haibara.</b>", null, "neutral"),
+        new Dialogue(haibara, "Alr then where do you wanna go now? ", 
+            new Choice([{
+                "text" : "Back to Kogoro pls",
+                "outcome" : () => {changeNextScene("MouriAgency")}
+            }, {
+                "text" : "Say that again?",
+                "outcome" : () => {changeNextScene("AgasaHouse")}
             }]
         ), "neutral"),
         new Dialogue(haibara, "Alright!", null, "neutral"),
