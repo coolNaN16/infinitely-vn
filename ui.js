14998a2da4d4c9657b8301a549b7988d51393788
coolNaN16 <kenzie.cool16.3@gmail.com>
2026-06-18 16:04:08 +0700

    added a blur effect on storage menu appear

--- ui.js
+++ ui.js
@@ -1,17 +1,23 @@
 function toggleStorageDisplay() {
+    const blur = document.getElementById("blur")
     const storage = document.getElementById("storageUI")
     const cpanel = document.getElementById("controlPanel")
     if (storage.style.display == "flex") {
         storage.style.translate = "-100% 0"
+        blur.style.opacity = 0
+
         setTimeout(function() {
             storage.style.display = "none"
+            blur.style.display = "none"
         }, 500)
         
     } else {
         storage.style.display = "flex"
-
+        blur.style.display = "block"
+        
         requestAnimationFrame(() => {
             storage.style.translate = "0 0"
+            blur.style.opacity = 1
         })
     }
 
